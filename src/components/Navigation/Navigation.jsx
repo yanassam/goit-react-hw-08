fa157cba43f6a412946a756a4d69b7253c9437d8
Yana <yanassam81@gmail.com>
2024-05-22 14:20:29 +0200

    fix navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,20 +1,27 @@
 import { NavLink } from "react-router-dom";
 import s from "./Navigation.module.css";
 import clsx from "clsx";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const buildLinkClass = ({ isActive }) => {
   return clsx(s.link, isActive && s.active);
 };
 
-const Navigation = () => (
-  <nav className={s.nav}>
-    <NavLink exact="true" to="/" className={buildLinkClass}>
-      Home
-    </NavLink>
-    <NavLink to="/contacts" className={buildLinkClass}>
-      Contacts
-    </NavLink>
-  </nav>
-);
+const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
+  return (
+    <nav className={s.nav}>
+      <NavLink to="/" className={buildLinkClass}>
+        Home
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink to="/contacts" className={buildLinkClass}>
+          Contacts
+        </NavLink>
+      )}
+    </nav>
+  );
+};
 export default Navigation;
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,15 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
-import { selectUserEmail } from "../../redux/auth/selectors";
+import {
+  selectIsLoggedIn,
+  selectUser,
+  selectUserEmail,
+} from "../../redux/auth/selectors";
 import { logOut } from "../../redux/auth/operations";
 import s from "./UserMenu.module.css";
 
 const UserMenu = () => {
   const dispatch = useDispatch();
-  const email = useSelector(selectUserEmail);
+  // const email = useSelector(selectUserEmail);
 
+  const user = useSelector(selectUser);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  if (!isLoggedIn) return null;
   return (
     <div className={s.userMenu}>
-      <span className={s.email}>Welcome, {email}!</span>
+      <span className={s.email}>Welcome, {user.name}!!</span>
       <button className={s.button} onClick={() => dispatch(logOut())}>
         Logout
       </button>
